495d9c8085b1353427b277b7c53331c3c9891150
adkibhaskar <121316425+adkibhaskar@users.noreply.github.com>
2023-02-17 22:02:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClPapvJD1VhJ_wjBzPh6j8_nzx26FTUDw",
+    authDomain: "loginform-63d78.firebaseapp.com",
+    projectId: "loginform-63d78",
+    storageBucket: "loginform-63d78.appspot.com",
+    messagingSenderId: "921012236669",
+    appId: "1:921012236669:web:fa382163fd2f09d57cdc14",
+    measurementId: "G-M5ZDM3YSW4"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
